6c5715808e53278a17355a90e6a5198bde92d3d7
CHETAN GOUR <chetangour88@gmail.com>
2023-05-12 11:42:15 -0700

    added cors

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ const bodyParser = require("body-parser");
 const path = require("path");
 const db = require("./config/db");
 
+// Enable CORS middleware for all origins
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 // load env vars
 dotenv.config({ path: "./config/config.env" });
 
